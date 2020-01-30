0e464073bdc9a05bcf050ccadcef20202dbf6f51
vpopov97 <56735311+vpopov97@users.noreply.github.com>
2020-01-30 14:26:07 +0300

    Delete processDeleteJobs.php

--- models/process/processDeleteJobs.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once $_SERVER['DOCUMENT_ROOT']."/models/Job_model.php";
-require_once $_SERVER['DOCUMENT_ROOT']."/controllers/Controller_Job.php";
-		
-		$action = "delete";
-		$controller = new Job;
-		$controller->$action();
-
- ?>
