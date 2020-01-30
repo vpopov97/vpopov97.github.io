1d3669ed17c734540186ce458c6bc69ebb447ee5
vpopov97 <56735311+vpopov97@users.noreply.github.com>
2020-01-30 14:25:57 +0300

    Delete processDelete.php

--- models/process/processDelete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once  $_SERVER['DOCUMENT_ROOT']."/models/Stuff_model.php";
-require_once $_SERVER['DOCUMENT_ROOT']."/controllers/Controller_main.php";
-		
-		$action = "delete";
-		$controller = new main;
-		$controller->$action();
-
- ?>
